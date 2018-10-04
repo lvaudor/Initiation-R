43620261f7258d6f0af58caf239612d3141128ed
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2018-10-04 17:23:00 +0200

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -3,7 +3,7 @@ library(remotes)
 ##### Install specific package versions from CRAN #####
 
 # install_version("ggplot2", "3.0.0")
-# install_version("dplyr", "0.7.5")
+install_version("dplyr", "0.7.5")
 # install_version("tidyr", "0.8.1")
 # install_version("purrr", "0.2.5")
 install_version("readr","1.1.1")
